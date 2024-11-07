dcbb67a9f5480758843ec78049b0f53742cefe83
zippykitche <zippyk80@gmail.com>
2024-11-07 13:15:38 +0300

    complete infomation flow lab

--- src/__tests__/ShoppingList.test.js
+++ src/__tests__/ShoppingList.test.js
@@ -2,6 +2,7 @@ import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
 import ShoppingList from "../components/ShoppingList";
 
+
 const testData = [
   { id: 1, name: "Yogurt", category: "Dairy" },
   { id: 2, name: "Pomegranate", category: "Produce" },
@@ -9,6 +10,7 @@ const testData = [
   { id: 4, name: "String Cheese", category: "Dairy" },
   { id: 5, name: "Cookies", category: "Dessert" },
 ];
+
 
 test("displays all items when initially rendered", () => {
   const { container } = render(<ShoppingList items={testData} />);
